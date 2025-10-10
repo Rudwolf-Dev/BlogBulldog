6cf0210e3c205fdb49c1b3bc837fefcf0692605e
Rudwolf-Dev <rodolfopemc@outlook.com>
2025-10-09 23:55:49 -0600

    1.6.0

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import info from '@/components/info.vue'
 import deportes from '@/components/deportes.vue'
 import actExt from '@/components/actExt.vue'
 import videoJuegos from '@/components/videoJuegos.vue'
+import holaSlenderman from '@/components/holaSlenderman.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,8 @@ const router = createRouter({
     { path: '/info', component: info },
     { path: '/actExt', component: actExt },
     { path: '/equiposRep', component: deportes },
-    { path: '/videoJuegos', component: videoJuegos }
+    { path: '/videoJuegos', component: videoJuegos },
+    { path: '/casiTodos', component: holaSlenderman }
   ],
 })
 
