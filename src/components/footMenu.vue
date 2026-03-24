c4bbf327630487303b58a0133f74b0b7a5b03dcd
Rudwolf <rodolfopemc@outlook.com>
2026-03-24 14:23:30 -0600

    xdxdxd

--- src/components/footMenu.vue
+++ src/components/footMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <img src="./../assets/img/EIAO-95Anv.jpg" alt="EIAO-Aniversario95" height="150px" />
-  <p class="pt-4 fw-semibold">2023 - 2026 - Equipo Rosaso</p>
+  <p class="fw-semibold opacity-25">Monrise Rudwolf</p>
+  <p class="pt-4 fw-semibold">2023 - 2026 -- Equipo Rosaso</p>
 </template>
 
 <style scoped>
