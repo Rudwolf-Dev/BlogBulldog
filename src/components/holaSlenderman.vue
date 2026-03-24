a1b948985cf648cb933833af369e360beab7ed34
Rudwolf <rodolfopemc@outlook.com>
2026-03-24 14:05:09 -0600

    XDXD

--- src/components/holaSlenderman.vue
+++ src/components/holaSlenderman.vue
@@ -1,5 +1,5 @@
 <template>
-  <video class=" mx-5 rounded-2 container-md" controls height="auto" width="auto">
+  <video class=" mx-5 rounded-2 container-md" controls height="auto" width="auto" poster="./../assets/img/poster.png">
   <source src="./../assets/img/ozuna.mp4" type="video/mp4">
   </video>
 </template>
